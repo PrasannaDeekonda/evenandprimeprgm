a805f3b64e619b27952a84b57bbd21839efbf8a1
Prasanna Deekonda <prasannadeekonda19@gmail.com>
2023-05-29 21:04:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/*using System;
+namespace Application
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int i=Convert.ToInt32(Console.ReadLine());
+            if(i%2==0)
+            {
+                Console.WriteLine("Given number is even");
+            }
+            else
+            {
+                Console.WriteLine("Given number is odd");
+            }
+        }
+    }
+}*/
+/*using System;
+namespace Application
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int i=Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for(int j=2;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    count++;
+                }
+            }
+            if(count==1)
+            {
+                Console.WriteLine("Given number is prime number");
+            }
+            else 
+            {
+                Console.WriteLine("Given number is not prime number");
+            }
+        }
+    }
+}*/
